b494ce2461eba8d1b770af5f7e7ac070688af189
Ryan <ryanong14@gmail.com>
2023-03-28 22:27:02 +0800

    add support for two patients

--- HealthcareWorker.py
+++ HealthcareWorker.py
@@ -2,11 +2,14 @@ import paho.mqtt.client as mqtt
 from threading import Thread
 from gpiozero import LED, Button
 from gpiozero.pins.pigpio import PiGPIOFactory
+import json
 
 class HealthcareWorker:
-    def __init__(self, led_pin, btn_pin, rpi_ip) -> None:
+    def __init__(self, led_pin, btn_pin, led_pin_2, btn_pin_2, rpi_ip) -> None:
         self.led_pin = led_pin
         self.btn_pin = btn_pin
+        self.led_pin_2 = led_pin_2
+        self.btn_pin_2 = btn_pin_2
         self.rpi_ip = rpi_ip
         self.init_mqtt()
         self.init_gpio()
@@ -15,20 +18,37 @@ class HealthcareWorker:
         while True:
             pass
 
-    def poll_button(self):
+    def poll_button(self, patient_name):
         # start button poll
-        self.btn.when_activated = self.on_button_press
+        self.btn.when_activated = lambda : self.on_button_press(patient_name)
 
         # keep looping when btn is not press and still has a callback function
         while not self.btn.is_active and self.btn.when_activated:
             pass
 
-    def on_button_press(self):
+    def poll_button_2(self, patient_name):
+        # start button poll
+        self.btn_2.when_activated = lambda : self.on_button_press_2(patient_name)
+
+        # keep looping when btn is not press and still has a callback function
+        while not self.btn_2.is_active and self.btn_2.when_activated:
+            pass
+
+    def on_button_press(self, patient_name):
             # disable button
             self.btn.when_activated = None
 
             self.led.off()
-            self.client.publish('to_patient', 'off')
+            topic = patient_name.replace(' ', '_')
+            self.client.publish(topic, 'off')
+
+    def on_button_press_2(self, patient_name):
+            # disable button
+            self.btn_2.when_activated = None
+
+            self.led_2.off()
+            topic = patient_name.replace(' ', '_')
+            self.client.publish(topic, 'off')
 
     def init_mqtt(self):
         self.client = mqtt.Client()
@@ -44,28 +64,46 @@ class HealthcareWorker:
         self.client.subscribe("to_hc_worker")
 
     def on_message(self, client, userdata, msg):
-        msg = msg.payload.decode()
+        msg = json.loads(msg.payload)
         
-        if msg == 'on':
-            self.led.on()
-            print('led on')
-            self.poll_thread = Thread(target=self.poll_button)
-            self.poll_thread.start()
+        if msg['cmd'] == 'led_on':
+            if msg['name'] == 'Sibei Suei':
+                self.led.on()
+                print('led on')
+                poll_thread = Thread(target=self.poll_button, args=(msg['name'],))
+                poll_thread.start()
+            elif msg['name'] == 'Sibei Sian':
+                self.led_2.on()
+                print('led_2 on')
+                poll_thread = Thread(target=self.poll_button_2, args=(msg['name'],))
+                poll_thread.start()
 
-        elif msg == 'off':
-            self.led.off()
-            print('led off')
+            
+            print('led on')
+
+        elif msg['cmd'] == 'led_off':
+            if msg['name'] == 'Sibei Suei':
+                self.led.off()
+                print('led off')
+            elif msg['name'] == 'Sibei Sian':
+                self.led_2.off()
+                print('led_2 off')
+            
 
     def init_gpio(self):
         if self.rpi_ip is None:
             self.led = LED(self.led_pin)
             self.btn = Button(self.btn_pin)
+            self.led_2 = LED(self.led_pin_2)
+            self.btn_2 = Button(self.btn_pin_2)
         else:
             factory = PiGPIOFactory(host=self.rpi_ip)
             self.led = LED(self.led_pin, pin_factory=factory)
             self.btn = Button(self.btn_pin, pin_factory=factory)
+            self.led_2 = LED(self.led_pin_2, pin_factory=factory)
+            self.btn_2 = Button(self.btn_pin_2, pin_factory=factory)
 
         print('gpio initialised')
     
-    def on_send_message(self, msg):
-        self.client.publish('to_patient', msg)
+    def on_send_message(self, topic, msg):
+        self.client.publish(topic, msg)
